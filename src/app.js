aee4c20c9a761de846f38a49dff7cfb0c65c5abf
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-01 15:27:32 -0300

    Buscar dados em uma API (Back-end)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 'use strict'
 
 import React, { Component } from 'react'
+import ajax from '@fdaciuk/ajax'
 import AppContent from './components/app-content'
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super()
     this.state = {
       userInfo: null,
@@ -13,12 +14,65 @@ class App extends Component {
     }
   }
 
-  render () {
+  handleSearch(e) {
+    const value = e.target.value
+    const keyCode = e.which || e.keyCode
+    const ENTER = 13
+
+    if (keyCode === ENTER) {
+      console.log(e.target.value)
+
+      ajax()
+        .get(this.getGitHubApiUrl(value))
+        .then(result => {
+          console.log(result)
+          this.setState({
+            userInfo: {
+              username: result.name,
+              photo: result.avatar_url,
+              login: result.login,
+              repos: result.public_repos,
+              followers: result.followers,
+              following: result.following
+            },
+            repos: [],
+            starred: []
+          })
+        })
+    }
+  }
+
+  getRepos(type) {
+    return () => {
+      const username = this.state.userInfo.login
+      ajax()
+        .get(this.getGitHubApiUrl(username, type))
+        .then(result => {
+          this.setState({
+            [type]: result.map(repo => ({
+              name: repo.name,
+              link: repo.html_url
+            }))
+          })
+        })
+    }
+  }
+
+  getGitHubApiUrl (username, type) {
+    const internalUser = username ? `/${username}` : ''
+    const internalType = type ? `/${type}` : ''
+    return `https://api.github.com/users${internalUser}${internalType}`
+  }
+
+  render() {
     return (
       <AppContent
-        userInfo={this.state.unserInfo}
+        userInfo={this.state.userInfo}
         repos={this.state.repos}
         starred={this.state.starred}
+        handleSearch={e => this.handleSearch(e)}
+        getRepos={this.getRepos('repos')}
+        getStarred={this.getRepos('starred')}
       />
     )
   }
--- src/components/app-content.js
+++ src/components/app-content.js
@@ -5,15 +5,22 @@ import UserInfo from './user-info'
 import Actions from './actions'
 import Repos from './repos'
 
-const AppContent = ({ userInfo, repos, starred }) => {
+const AppContent = ({
+  userInfo,
+  repos,
+  starred,
+  handleSearch,
+  getRepos,
+  getStarred
+}) => {
   return (
     <div>
       <div className='app'>
-        <Search />
+        <Search handleSearch={handleSearch} />
 
         {!!userInfo && <UserInfo userInfo={userInfo} />}
 
-        {!!userInfo && <Actions />}
+        {!!userInfo && <Actions getRepos={getRepos} getStarred={getStarred} />}
 
         {!!repos.length && (
           <Repos className='repos' title='Repositórios:' repos={repos} />
