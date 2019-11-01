4c061781386bd11d19e7217ce351072535a157e7
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-01 11:06:15 -0300

    45.M1#35 Criando uma marcação da applicação

--- src/app.js
+++ src/app.js
@@ -2,10 +2,50 @@
 
 import React from 'react'
 
-const App = React.createClass({
-  render: function () {
-    return <h1>Aplicação!!!</h1>
-  }
-})
+const App = () => (
+  <div className='app'>
+    <div className='search'>
+      <input type='search' placeholder='Digite o nome do usuário no GitHub' />
+    </div>
+
+    <div className='user-info'>
+      <img src='https://avatars.githubusercontent.com/u/15021804?v=4' />
+      <h1>
+        <a href='https://github.com/jeanbarcellos'>Jean Barcellos</a>
+      </h1>
+
+      <ul className='repos-info'>
+        <li>Repositórios: 122</li>
+        <li>Seguidores: 10</li>
+        <li>Seguindo: 10</li>
+      </ul>
+
+      <div className='actions'>
+        <button>Ver repositórios</button>
+        <button>Ver favoritos</button>
+      </div>
+
+      <div className='repos'>
+        <h2>Repositórios:</h2>
+        <ul>
+          <li>
+            <a href='#'>Nome do repositório</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className='starred'>
+        <h2>Favoritos:</h2>
+        <ul>
+          <li>
+            <a href='#'>Nome do repositório</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+)
 
 export default App
+
+// https://api.github.com/users/jeanbarcellos
