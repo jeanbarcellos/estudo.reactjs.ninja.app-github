e3f1f284b19f95a42604d16eaf3d8924afb2a467
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-05 09:25:38 -0300

    Webpack: gerar bundle de produção

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -8,24 +8,32 @@ const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = validate({
-  devtool: 'source-map',
-
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, 'src', 'index')
-  ],
+  entry: path.join(__dirname, 'src', 'index'),
 
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name]-[hash].js',
-    publicPath: ''
+    filename: '[name]-[hash].js'
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new ExtractTextPlugin('[name]-[hash].css'),
+
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    }),
+
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    }),
+
+    // Não deixa duplicar arquivos chamados novamente
+    new webpack.optimize.DedupePlugin(),
+
+    // Ordena para carregar os mais leves primeiro
+    new webpack.optimize.OccurrenceOrderPlugin(),
+
     new HtmlPlugin({
       title: 'GitHub app',
       template: path.join(__dirname, 'src', 'html', 'template.html')
@@ -53,7 +61,7 @@ module.exports = validate({
         test: /\.css$/,
         exclude: /node_modules/,
         include: /src/,
-        loaderes: ['style', 'css']
+        loader: ExtractTextPlugin.extract('style', 'css')
       }
     ]
   }
