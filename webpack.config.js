62cef32e256098a1838957357086397c1ec18d74
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-04 22:05:10 -0300

    Format webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,49 +5,49 @@ const webpack = require('webpack')
 const validate = require('webpack-validator')
 
 module.exports = validate({
+  devtool: 'source-map',
 
-    devtool: 'source-map',
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, 'src', 'index')
+  ],
 
-    entry: [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        path.join(__dirname, 'src', 'index')
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        loader: 'standard'
+      }
     ],
 
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/dist/'
-    },
-
-    plugins: [
-        new webpack.HotModuleReplacementPlugin
-    ],
-
-    module: {
-        preLoaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            include: /src/,
-            loader: 'standard'
-        }],
-
-        loaders: [{
-          test: /\.js$/,
-          exclude: /node_modules/,
-          include: /src/,
-          loader: 'babel'
-        },
-        {
-          test: /\.css$/,
-          exclude: /node_modules/,
-          include: /src/,
-          loaders: ['style', 'raw']
-        }]
-    }
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        include: /src/,
+        loaders: ['style', 'raw']
+      }
+    ]
+  }
 })
-
 
 /*
 entry
