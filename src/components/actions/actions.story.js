b5e45e7290e23f1b033c985da1c1b71e5e07c54c
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-05 15:54:23 -0300

    M2#A43 - Configurando o Storybook

---
+++ src/components/actions/actions.story.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import { storiesOf, action } from '@kadira/storybook'
+import React from 'react'
+
+const stories = storiesOf('Actions', module)
+
+stories.add('Actions component', () => (
+  <div>Hello</div>
+))
