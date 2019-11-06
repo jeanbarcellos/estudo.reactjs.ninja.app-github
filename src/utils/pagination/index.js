7cb31ea9ee9d07ff613d169ac85ed2a04881a4ae
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-06 14:53:29 -0300

    M2#A57 - Paginação - testando as excessões

--- src/utils/pagination/index.js
+++ src/utils/pagination/index.js
@@ -12,7 +12,16 @@ const centerRule = ({ total, activePage }) => {
   return activePage - 1
 }
 
-const pagination = ({ total, activePage }) => {
+const pagination = ({ total = 1, activePage = 1 } = {}) => {
+
+  if (typeof total !== 'number') {
+    throw new TypeError('total should be a number')
+  }
+
+  if (typeof activePage !== 'number') {
+    throw new TypeError('activePage should be a number')
+  }
+
   if (total <= 5) {
     return Array.from({ length: total }, (_, i) => i + 1)
   }
