a70dd8961b5181201cd1ea22b5a37ff877f5184e
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-06 15:48:58 -0300

    M2#A58 - Paginação - storybook

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const path = require('path')
 const webpackConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js')
 
 module.exports = function (config, env) {
@@ -10,6 +11,15 @@ module.exports = function (config, env) {
     exclude: /node_modules/,
     loader: 'standard'
   })
+
+
+  newConfig.resolve = {
+    alias: {
+      src: path.join(__dirname, '..', 'src'),
+      components: path.join(__dirname, '..', 'src', 'components'),
+      utils: path.join(__dirname, '..', 'src', 'utils')
+    }
+  }
 
   return newConfig
 }
