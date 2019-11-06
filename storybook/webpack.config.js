10205e1b390097c0c6b6484089796cecceb43ac1
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-06 17:15:05 -0300

    M2#A64 - Abstraindo configuração padrão do webpack - parte 3 Ajustando storybook

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,25 +1,13 @@
 'use strict'
 
-const path = require('path')
+const common = require('../webpack/common')
 const webpackConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js')
 
 module.exports = function (config, env) {
   const newConfig = webpackConfig(config, env)
 
-  newConfig.module.preLoaders = (newConfig.module.preLoaders || []).concat({
-    test: /\.js$/,
-    exclude: /node_modules/,
-    loader: 'standard'
-  })
-
-
-  newConfig.resolve = {
-    alias: {
-      src: path.join(__dirname, '..', 'src'),
-      components: path.join(__dirname, '..', 'src', 'components'),
-      utils: path.join(__dirname, '..', 'src', 'utils')
-    }
-  }
+  newConfig.module.preLoaders = (newConfig.module.preLoaders || []).concat(common.standardPreLoader)
+  newConfig.resolve = common.resolve
 
   return newConfig
 }
